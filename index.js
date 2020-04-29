15f3314fb777fef60e5eb79ec7f6ce1c95ac5e31
Elan Nurjailani <onexoxo15@gmail.com>
2020-04-29 11:48:57 +0700

    post and get data

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
-// create express app
 const app = express();
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
+const books = [
+    {id: 1, name: "comic"},
+    {id: 2, name: "ilmu"},
+    {id: 3, name: "majalah"},
+]
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.send('Hello Elan!!!');
 });
 
-// listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+// define a simple route
+app.get('/api/books', (req, res) => {
+    res.send(books);
 });
+
+app.post('/api/books',(req, res) => {
+    if (!req.body.name || req.body.name.length < 3){
+        // 400 Bad request
+        res.status(400).send('name is required and should be minimum 3 character');
+        return;
+    }
+    
+    const book = {
+        id: books.length + 1,
+        name: req.body.name
+    };
+    books.push(book);
+    res.send(book);
+});
+
+app.get('/api/books/:id', (req, res) => {
+    const book = books.find(c => c.id === parseInt(req.params.id));
+    if (!book) res.status(404).send('The book with the given ID was not found.'); //404
+    res.send(book);
+});
+
+// listen for requests port
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// app.post()
+// app.put()
+// app.delete()
